f60f13fe7b2e3a6803b90a0329af5f78304fc8cf
hugobaraujo <120087343+hugobaraujo@users.noreply.github.com>
2023-03-21 11:36:19 +0100

    Update main.py
    Updated for genetic algorithm and simulated annealing tests

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib as plt
 import fil_rouge_tools as frt
+
 import ga_tools as ga
+import recuit
+
 
 clients  = frt.get_clients()
-clients = clients[0:100] # creates a smaller version of the database, for testing
+#clients = clients[0:100] # creates a smaller version of the database, for testing
 
 # specify a solution by creating a numpy array with the same number of elements
 #       as there are clients in the problem
@@ -16,8 +19,19 @@ best_solution = np.copy(solution)
 
 iterations = 0  # iteractions counter
 
-ga.genetic_algorithm(clients, 100, clients.shape[0], 1000, 15)
+# genetic algorithm test
+ga_sol, ga_fit = ga.genetic_algorithm(clients, 100, clients.shape[0], 1000, 15)
+print(ga_sol)
+frt.improvement_curve(ga_fit, "Genetic Algorithm")
 
+# recuit simulé test
+recuit_sol, recuit_fit = recuit.recuit_simule(clients, best_solution, best)
+print(recuit_sol)
+frt.improvement_curve(recuit_fit, "Recuit Simulé")
+
+# tabou test
+
+# random algorithm test
 while(1):
 
     solution = np.copy(best_solution)
